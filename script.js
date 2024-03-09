287afbcea61526c3d07ff9f43ca2581941369cc1
AYUSH KUMAR <125579641+ayushmarch94@users.noreply.github.com>
2024-03-09 17:44:48 +0530

    Created basic structure of website

---
+++ script.js
@@ -0,0 +1,12 @@
+let hamburger=document.getElementById('hamburger');
+let closeLinks=document.getElementById('closeLinks');
+
+hamburger.addEventListener('click',()=>{
+    closeLinks.style.display='flex';
+    
+});
+
+
+
+
+
